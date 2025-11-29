8c30e63192d626fb178f241445c66af151b2e223
nirajan1 <nirajanjoshi80@gmail.com>
2025-11-29 12:06:01 +0545

    refactor/transction trashed+ force delete+ restore added

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\TransactionResource;
 use App\Models\Category;
+use App\Models\PaymentMethod;
 use App\Models\Transaction;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+use function Symfony\Component\Clock\now;
 
 class TransactionController extends Controller
 {
@@ -29,110 +31,6 @@ class TransactionController extends Controller
         ], 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        $userId = Auth::id();
-        $validated = $request->validate([
-            'uuid' => 'required|uuid',
-            'category_id' => 'required|exists:categories,id',
-            'payment_method_id' => 'required|exists:payment_methods,id',
-            'transaction_amount' => 'required|numeric|min:0',
-            'transaction_date' => 'required|date',
-            'client_updated_at' => 'required|date'
-        ]);
-        $category = Category::with(['user'])->findOrFail($validated['category_id']);
-
-        //  category is NOT global AND does NOT belong to the current user
-        if (!$category->is_global && $category->user_id !== $userId) {
-            return response()->json([
-                'response' => '401',
-                'message' => 'Invalid category (no access)',
-            ], 401);
-        }
-        $transaction = Transaction::create([
-            'uuid' => $validated['uuid'],
-            'user_id' => $userId,
-            'category_id' => $validated['category_id'],
-            'payment_method_id' => $validated['payment_method_id'],
-            'transaction_amount' => $validated['transaction_amount'],
-            'transaction_date' => $validated['transaction_date'],
-            'client_updated_at' => $validated['client_updated_at'],
-        ]);
-
-        return response()->json([
-            'response' => '200',
-            'message' => 'Transaction created successfully.',
-            'data' => new TransactionResource($transaction),
-        ]);
-    }
-
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $uuid)
-    {
-        $transaction = Transaction::where('uuid', $uuid)
-            ->where('user_id', Auth::id())
-            ->with(['category', 'paymentMethod'])
-            ->first();
-
-        if (!$transaction) {
-            return response()->json([
-                'response' => '404',
-                'message' => 'Transaction not found.',
-                'data' => [],
-            ], 404);
-        }
-
-        return response()->json([
-            'response' => '200',
-            'message' => 'Transaction  fetched successfully.',
-            'data' => new TransactionResource($transaction),
-        ], 200);
-    }
-    /**
-     * Update a newly created resource in storage.
-     */
-    public function update(Request $request, $uuid)
-    {
-        $userId = Auth::id();
-
-        $transaction = Transaction::where('uuid', $uuid)
-            ->where('user_id', $userId)
-            ->with(['category', 'paymentMethod'])
-            ->firstOrFail();
-
-
-        $validated = $request->validate([
-            'uuid' => 'required|uuid',
-            'category_id' => 'required|exists:categories,id',
-            'payment_method_id' => 'required|exists:payment_methods,id',
-            'transaction_amount' => 'required|numeric|min:0',
-            'transaction_date' => 'required|date',
-            'client_updated_at' => 'required|date'
-        ]);
-        $category = Category::with(['user'])->findOrFail($validated['category_id']);
-
-        //  category is NOT global AND does NOT belong to the current user
-        if (!$category->is_global && $category->user_id !== $userId) {
-            return response()->json([
-                'response' => '401',
-                'message' => 'Invalid category (no access)',
-            ], 401);
-        }
-
-        $transaction->update($validated);
-
-        return response()->json([
-            'response' => '200',
-            'message' => 'Transaction Updated successfully.',
-            'data' => new TransactionResource($transaction),
-        ]);
-    }
     /**
      * Offline-first sync endpoint
      */
@@ -176,9 +74,16 @@ class TransactionController extends Controller
             if (Transaction::where('uuid', $item['uuid'])->exists()) continue;
 
             // check category
-            $category = Category::findOrFail($item['category_id']);
-            if (!$category) continue;
-            if (!$category->is_global && $category->user_id != $userId) continue;
+            $category = Category::find($item['category_id']);
+            if (!$category || (!$category->is_global && $category->user_id !== $userId)) {
+                continue;
+            }
+
+            $paymentMethod = PaymentMethod::find($item['payment_method_id']);
+            if (!$paymentMethod || $paymentMethod->user_id !== $userId) {
+                continue;
+            }
+
 
             $transaction = Transaction::create([
                 'uuid' => $item['uuid'],
@@ -205,12 +110,20 @@ class TransactionController extends Controller
             if (!$transaction) continue;
 
             // category permission check for security
-            $category = Category::findOrFail($item['category_id']);
-            if (!$category) continue;
-            if (!$category->is_global && $category->user_id != $userId) continue;
+            $category = Category::find($item['category_id']);
+            if (!$category || (!$category->is_global && $category->user_id !== $userId)) {
+                continue;
+            }
+
+            $paymentMethod = PaymentMethod::find($item['payment_method_id']);
+            if (!$paymentMethod || $paymentMethod->user_id !== $userId) {
+                continue;
+            }
+            $clientTime = strtotime($item['client_updated_at']);
+            $serverTime = strtotime($transaction->client_updated_at);
 
             // Conflict resolution-> only update if client has newer updated_at
-            if (!$transaction->client_updated_at   || strtotime($item['client_updated_at']) > strtotime($transaction->client_updated_at)) {
+            if (!$transaction->client_updated_at   || $clientTime > $serverTime) {
                 $transaction->update([
                     'category_id' => $item['category_id'],
                     'payment_method_id' => $item['payment_method_id'],
@@ -229,7 +142,7 @@ class TransactionController extends Controller
         if (!empty($validated['deleted'])) {
             $toDelete =   Transaction::whereIn('uuid', $validated['deleted'])
                 ->where('user_id', $userId)
-                ->get(); // soft delete
+                ->get();
 
             foreach ($toDelete as $t) {
                 if (!$t->deleted_at) {
@@ -261,6 +174,57 @@ class TransactionController extends Controller
                 'changes' => TransactionResource::collection($changedData),
                 'server_time' => Carbon::now()->toIso8601String(),
             ],
+        ]);
+    }
+
+    /**
+     * Display all trashed items only.
+     */
+
+    public function trashed()
+    {
+        $trashed = Transaction::onlyTrashed()
+            ->where('user_id', Auth::id())
+            ->with(['category', 'paymentMethod'])
+            ->get();
+
+        return response()->json([
+            'response' => 200,
+            'message' => 'Trashed transactions fetched.',
+            'data' => TransactionResource::collection($trashed)
+        ]);
+    }
+
+    // restore trashed items
+    public function restoreTrashed($uuid)
+    {
+
+        $transaction = Transaction::withTrashed()
+            ->where('uuid', $uuid)
+            ->where('user_id', Auth::id())
+            ->firstOrFail();
+
+        $transaction->restore();
+
+        $transaction->update(['client_updated_at' => now()]);
+        return response()->json([
+            'response' => 200,
+            'message' => 'Transaction restored successfully.',
+            'data' => new TransactionResource($transaction),
+        ]);
+    }
+    public function forceDelete($uuid)
+    {
+        $transaction = Transaction::withTrashed()
+            ->where('uuid', $uuid)
+            ->where('user_id', Auth::id())
+            ->firstOrFail();
+
+        $transaction->forceDelete();
+        return response()->json([
+            'response' => 200,
+            'message' => 'Transaction permanently deleted.',
+            'uuid' => $uuid
         ]);
     }
 }
